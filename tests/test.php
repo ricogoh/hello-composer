a85f816d644f7af70e60f48797e346c1c68f7335
ricogoh <ricogoh@gmail.com>
2017-06-01 12:04:55 +0800

    change namespace

--- tests/test.php
+++ tests/test.php
@@ -2,10 +2,12 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use ricogoh\Hello;
-use ricogoh\Greetings;
+use RicoGoh\Hello;
+use RicoGoh\Greetings;
 
 $hello = new Hello();
 
 echo $hello->say("Hello World");
 echo Greetings::say("Hello World");
+
+echo RicoGoh\Greetings::say("Bye");
